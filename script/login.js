c77eac818de588935bfa774ec0688fb4ee5aa760
zaberabdullah <zaberabdullah120@gmail.com>
2026-03-07 15:07:56 +0600

    added the navbar section by using DaisyUI

---
+++ script/login.js
@@ -0,0 +1,17 @@
+// console.log("Hello from js")
+
+const loginForm = document.getElementById('loginForm');
+loginForm.addEventListener('submit', function(event){
+    event.preventDefault();
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+    
+if (username === 'admin' && password === 'admin123'){
+    alert('Welcome! Login Successful.');
+    window.location.assign("/home.html")
+
+}else{
+    alert('Invalid credentials. Please use admin/admin123');
+}
+
+});

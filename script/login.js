284b0ba59590037d5cdf4cfa324df23e3c65273a
zaberabdullah <zaberabdullah120@gmail.com>
2026-03-16 03:13:32 +0600

    window.location issue.....

--- script/login.js
+++ script/login.js
@@ -1,17 +1,15 @@
 // console.log("Hello from js")
 
-const loginForm = document.getElementById('loginForm');
-loginForm.addEventListener('submit', function(event){
-    event.preventDefault();
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
-    
-if (username === 'admin' && password === 'admin123'){
-    alert('Welcome! Login Successful.');
-    window.location.assign("../home.html")
+const loginForm = document.getElementById("loginForm");
+loginForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
 
-}else{
-    alert('Invalid credentials. Please use admin/admin123');
-}
-
+  if (username === "admin" && password === "admin123") {
+    alert("Welcome! Login Successful.");
+    window.location.assign("home.html");
+  } else {
+    alert("Invalid credentials. Please use admin/admin123");
+  }
 });
